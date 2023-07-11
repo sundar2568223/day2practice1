298f1573604b35f733010fbcde96898a9a477d1d
sunder2568223 <sunderyssr@gmail.com>
2023-07-11 10:21:24 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Consolepractice01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int ID;
+            char gender;
+            DateTime doj;
+            double salary;
+            Console.WriteLine("Enter ID");
+            ID = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Empolyee Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Employee Gender");
+            gender = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Empolyee Salary");
+            salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Date of Joining");
+            doj = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("ID : \t" + ID  + "\n Employee Name: \t" + name + "\n Empolyee Gender: \t"
+                + gender + "\n Empolee Salary: \t  " + salary + "\n DOJ: \t " + doj);
+            Console.ReadKey();
+            if (salary > 90000)
+            {
+                Console.WriteLine("you have to pay 30% tax");
+            }
+            else
+            {
+                Console.WriteLine("you have to pay 15% tax");
+            }
+        }
+    }
+}
